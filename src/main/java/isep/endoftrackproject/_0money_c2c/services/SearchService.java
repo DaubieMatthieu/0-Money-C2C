40376a2e99b42052d274591ffaebc468f5aabd23
GonzagueD <49749926+GonzagueD@users.noreply.github.com>
2022-06-02 22:28:48 +0200

    Update SearchService.java

--- src/main/java/isep/endoftrackproject/_0money_c2c/services/SearchService.java
+++ src/main/java/isep/endoftrackproject/_0money_c2c/services/SearchService.java
@@ -3,6 +3,10 @@ package isep.endoftrackproject._0money_c2c.services;
 import isep.endoftrackproject._0money_c2c.model.Item;
 import isep.endoftrackproject._0money_c2c.model.Search;
 import isep.endoftrackproject._0money_c2c.repositories.ItemRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,6 +20,28 @@ public class SearchService {
         Iterable<Item> searchResults = itemRepository.
                 search(search.getPriceMin(), search.getPriceMax(), search.getLatitude(), search.getLongitude(), search.getMaxDistance(), search.getAvailable(), search.getAcceptedQualities());
         //TODO Ranking -> return an ordered list
+
+        //using ArrayList to order the items
+        List<Item> temp = new ArrayList<Item>();
+        searchResults.forEach(temp::add);
+
+        
+        //Classement dans l'ordre décroissant du score
+
+        for(int i = 0; i< temp.size(); i++)
+        { 
+            for(int j = 0; j > temp.size()-1; j++)
+            {
+                if(temp.get(j+1).getscore()<temp.get(j).getscore())
+                {
+                    Item tempItem =temp.get(j+1);
+                    temp.set(j+1, temp.get(j));
+                    temp.set(j, tempItem);  
+                }
+            }
+        }
+        //On applique la list trié par score décroissant au resultat final
+        searchResults = temp;
         return searchResults;
     }
 }
